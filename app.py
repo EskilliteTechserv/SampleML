22dea79e479f7e440216f61b4970dd75111ffc95
EskilliteTechServ <info@eskillite.in>
2025-09-14 23:25:27 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import joblib
+import numpy as np
+import joblib
+joblib.dump(model, "classifier.pkl")
+model = joblib.load("classifier.pkl")
+st.title("iris pedictor")
+st.write("Enter the following details:")
+s_l = float(st.number_input("sepal length", min_value=1, max_value=5))
+s_w = float(st.number_input("sepal width", min_value=1, max_value=5))
+p_l = float(st.number_input("petal length", min_value=1, max_value=5))
+p_w = float(st.number_input("petal width", min_value=1, max_value=5))
+if st.button("Predict"):
+ features = np.array([[s_l,s_w,p_l,p_w]])
+ prediction = pipmodel.predict(features)
+ st.write(f"flower: {prediction[0]}")
